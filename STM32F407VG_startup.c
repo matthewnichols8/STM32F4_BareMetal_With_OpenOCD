65027ce7bbc02a56b414dac993dc3826ff64360e
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2025-04-02 22:34:37 -0400

    Added RAM size definitions and Stack definitions

--- STM32F407VG_startup.c
+++ STM32F407VG_startup.c
@@ -1,1 +1,7 @@
 #include <stdint.h>
+
+#define SRAM_START      0x20000000U
+#define SRAM_SIZE       (128 * 1024) //128kB
+#define SRAM_END        ( (SRAM_START) + (SRAM_SIZE) )
+
+#define STACK_START     SRAM_END
