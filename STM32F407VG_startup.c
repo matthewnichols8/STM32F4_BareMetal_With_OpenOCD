8c4a874b36312b0cf6f5c91c8c760016cad8fa04
Matthew Nichols <125522815+matthewnichols8@users.noreply.github.com>
2025-04-04 17:53:32 -0400

    Update STM32F407VG_startup.c
    Adding NMI_Handler()

--- STM32F407VG_startup.c
+++ STM32F407VG_startup.c
@@ -6,14 +6,20 @@
 
 #define STACK_START     SRAM_END
 
-void Reset_handler(void);
+void Reset_Handler(void);
+void NMI_Handler();
 
-uint32_t vectors[] = {
+uint32_t vectors[] __attribute__ ((section (".isr_vector"))) = {
     STACK_START,
-    (uint32_t) &Reset_handler,
+    (uint32_t) &Reset_Handler,
+    (uint32_t) &NMI_Handler,
 
 
 };
+
+void NMI_Handler() {
+
+}
 
 void Reset_handler() {
 
